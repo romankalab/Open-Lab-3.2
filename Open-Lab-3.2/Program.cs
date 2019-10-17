a55495581363e5f92235db0844c18a469779c44d
romankalab <roan.kalab@mail.com>
2019-10-17 16:04:08 +0200

    Initial

---
+++ Open-Lab-3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 2; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    string[,] array = new string[,]
+                    {
+                        {"cat", "dog", "fish"},
+                        {"bird", "horse", "cow"},
+                    };
+                    Console.Write($"{array[row, column]}\t");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
